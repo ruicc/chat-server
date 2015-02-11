b79d94261262259f391815a417f7fef1192d56d8
Kousuke Ruichi <ruicc.rail@gmail.com>
2015-02-12 00:34:06 +0900

    Add ekg.

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,15 @@ module Main where
 
 import           App.Prelude
 import           Chat (runChatServer)
+import           System.Remote.Monitoring
 
 port :: Int
 port = 3000
 
 main :: IO ()
-main = runChatServer port
+main = do
+    -- ekg: access at http://localhost:3001 with browser or curl.
+    -- curl -H "Accept: application/json" http://localhost:3001/
+    forkServer "localhost" 3001 
+
+    runChatServer port
