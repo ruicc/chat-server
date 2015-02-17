1ac27f234c39765aee02193f48f6b22da71194bd
Kousuke Ruichi <ruicc.rail@gmail.com>
2015-02-17 21:46:23 +0900

    Make code clean.

--- src/Cont.hs
+++ src/Cont.hs
@@ -8,80 +8,44 @@ import Control.Exception
 import System.IO
 import Data.Monoid
 
---type Concurrent r a = ContT r (StateT (a -> IO r) IO) a
---type Concurrent a = ContT () (StateT (a -> IO ()) IO) a
 type Concurrent a = ContT () IO a
 
 runConcurrent :: (a -> IO ()) -> Concurrent a -> IO ()
---runConcurrent cont action = flip evalStateT cont $ runContT action (liftIO . cont)
 runConcurrent cont action = runContT action (liftIO . cont)
-
---instance MonadState Concurrent (a -> IO ()) where
-
---main :: IO ()
---main = runConcurrent putStrLn $ do
---    liftIO $ getLine
---    path <- liftIO $ getLine
---    hdl <- liftIO $ openFile path ReadMode
 
 
 main :: IO ()
 main = runConcurrent putStrLn $ do
-    path <- liftIO $ getLine
-    hdl <- liftIO $ openFile path ReadMode
-
     let
-         errorhandler :: SomeException -> Concurrent String
-         errorhandler e = do
-             liftIO $ hClose hdl
-             liftIO $ putStrLn $ "Err: " <> show e
-             liftIO $ putStrLn "Error occured, handle closed normally."
-             return "Exit!!"
+        loop = do
+            path <- liftIO $ getLine
+            hdl <- liftIO $ openFile path ReadMode
 
-    exitStr <- handleM errorhandler $ do
-        str <- dosomething hdl
-        str1 <- dosomething hdl
-        str2 <- dosomething hdl
+            let
+                 errorhandler :: SomeException -> Concurrent String
+                 errorhandler e = do
+                     liftIO $ hClose hdl
+                     liftIO $ putStrLn $ "Err: " <> show e
+                     liftIO $ putStrLn "Error occured, handle closed normally."
+                     return "Exit!!"
 
---    exitStr <- callCC $ \ exit -> do
---        str <- dosomething hdl `catchM` \ (e :: SomeException) -> do
---            -- Cleanup
---            liftIO $ hClose hdl
---            liftIO $ putStrLn $ "Err: " <> show e
---            liftIO $ putStrLn "Error occured, handle closed normally."
-----            return "Exit!!"
---            exit "Exit!!"
-        liftIO $ putStrLn str
-        liftIO $ putStrLn str1
-        liftIO $ putStrLn str2
-        errorOccur str
-        return "ExitCont??"
---        return $ str ++ "\n" ++ str
-    liftIO $ putStrLn $ "Exit string: " <> exitStr
+            exitStr <- handleM errorhandler $ do
+                str <- dosomething hdl
+                str1 <- dosomething hdl
+                str2 <- dosomething hdl `onExceptionM` (liftIO $ putStrLn "Log: onExceptionM 2")
 
-    return "String for cont"
+                liftIO $ putStrLn str
+                liftIO $ putStrLn str1
+                liftIO $ putStrLn str2
+                errorOccur str `onExceptionM` (liftIO $ putStrLn "Log: onExceptionM 1")
+                return "ExitCont??"
+            liftIO $ putStrLn $ "Exit string: " <> exitStr
 
+            return "String for cont"
 
+            loop
+    loop
 
-catchM :: Exception e => Concurrent a -> (e -> Concurrent a) -> Concurrent a
-catchM action handler =
-    callCC $ \exit -> do
-        ContT $ \cont -> runContT action cont
-                    `catch` \e -> runContT (handler e) (\a -> runContT (exit a) return)
---catchM action handler =
---    callCC $ \exit -> do
---        ContT $ \cont -> do -- StateT
---            StateT $ \s -> do -- IO
---                runStateT (runContT action cont) s
---                    `catch` \e -> runStateT (runContT (handler e) (\a -> runContT (exit a) return)) s
-
-handleM :: Exception e => (e -> Concurrent a) -> Concurrent a -> Concurrent a
-handleM = flip catchM
-
---onExceptionM :: Concurrent a -> Concurrent b -> Concurrent a
---onExceptionM action handler = action `catchM` \ (e :: SomeException) -> do
---    _ <- handler
---    liftIO $ throwIO e
 
 dosomething :: Handle -> Concurrent String
 dosomething hdl = do
@@ -93,16 +57,35 @@ errorOccur str = do
     liftIO $ putStrLn str
     return str
 
---wedge :: Concurrent a -> Concurrent a
---wedge action = do
---    oldCont <- lift get
---    res <- callCC $ \ exit -> do
---        lift $ put exit
---        action
---    lift $ put oldCont
---    return res
+------------------------------------------------------------------------------------------
 
---forkFinally :: Concurrent a a -> (Either SomeException a -> Concurrent () ()) -> Concurrent () ThreadId
+catchM :: Exception e => Concurrent a -> (e -> Concurrent a) -> Concurrent a
+catchM action handler =
+    callCC $ \exit -> do
+        ContT $ \cont -> runContT action cont
+                    `catch` \e -> runContT (handler e) (\a -> runContT (exit a) return)
+
+handleM :: Exception e => (e -> Concurrent a) -> Concurrent a -> Concurrent a
+handleM = flip catchM
+
+onExceptionM :: Concurrent a -> Concurrent b -> Concurrent a
+onExceptionM action handler =
+    action `catchM` \ (e :: SomeException) -> do
+        _ <- handler
+        liftIO $ throwIO e
+
+forkFinally :: Concurrent a -> (Either SomeException a -> Concurrent ()) -> Concurrent ThreadId
+forkFinally = undefined
+
+
+--forkFinally :: Concurrent a -> (Either SomeException a -> Concurrent ()) -> Concurrent ThreadId
+--forkFinally action handler = ContT $ \cont -> do -- IO
+--    void $ Conc.forkFinally
+--                (runContT action cont)
+--                (\e ->  runContT (handler e) cont)
+--                (\e ->  runContT (handler e) (liftIO . exit))
+--    cont tid
+
 --forkFinally action handler = ContT $ \cont -> do -- StateT
 --    tid <- StateT $ \exit -> do -- IO
 --        tid <- Conc.forkFinally
